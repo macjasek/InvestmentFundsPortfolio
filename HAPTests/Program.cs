56f75adc0be48f14f2f51620c0da7875074c88a0
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-02-03 20:46:36 +0100

    Testing method to scrape data from web using HtmlAgilityPack

---
+++ HAPTests/Program.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace HAPTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = @"https://www.bankier.pl/fundusze/notowania/UNI34";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//tr[1]/td[2]");
+
+            //var nodes = htmlDoc.DocumentNode.SelectNodes("//td");
+
+            //foreach (var singleNode in nodes)
+            //{
+            //    if (singleNode.HasClass("textBold"))
+            //    {
+            //        Console.WriteLine(singleNode.InnerText.Trim());
+            //    }
+
+            //}
+            Console.WriteLine(node.InnerText);
+
+        }
+    }
+}
