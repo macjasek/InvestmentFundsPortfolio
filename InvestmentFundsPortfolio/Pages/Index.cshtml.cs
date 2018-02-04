dcf9507689f81aa4d069f9d9cccd9239b2f7c9fd
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-02-04 20:02:28 +0100

    Added Price and Name of the fund to the Model. Working on user scenario and list of necessary features.

--- InvestmentFundsPortfolio/Pages/Index.cshtml.cs
+++ InvestmentFundsPortfolio/Pages/Index.cshtml.cs
@@ -1,16 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InvestmentFundsPortfolio.Pages
 {
     public class IndexModel : PageModel
     {
-        
+        [BindProperty]
+        public string Price { get; set; }
+        public string Name { get; set; }
 
         public void OnGet()
         {
-            
 
-           
+            var html = @"https://www.bankier.pl/fundusze/notowania/UNI34";
+            html = @"https://www.bankier.pl/fundusze/notowania/QRS17";
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//tr[1]/td[2]");
+
+            Price = node.InnerText.Trim().Split('&')[0];
+
+            node = htmlDoc.DocumentNode.SelectSingleNode("//div/h1");
+
+            Name = node.InnerText;
 
         }
     }
