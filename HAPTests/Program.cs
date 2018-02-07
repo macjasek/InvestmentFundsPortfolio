59c3eeef30dd38e0c4356e13d5afad49b7c94bbf
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-02-07 20:20:29 +0100

    The possibility of collecting fund quotations from another source was tested.

--- HAPTests/Program.cs
+++ HAPTests/Program.cs
@@ -7,13 +7,18 @@ namespace HAPTests
     {
         static void Main(string[] args)
         {
-            var html = @"https://www.bankier.pl/fundusze/notowania/UNI34";
+            // var html = @"https://www.bankier.pl/fundusze/notowania/UNI34";
+
+           var html = @"https://www.analizy.pl/fundusze/fundusze-inwestycyjne/profil-funduszu/OPT04/89th-Avenue-Fund-No-1-FIZ.html";
+            html = @"https://www.analizy.pl/fundusze/fundusze-zagraniczne/profil-funduszu/BGF054_A2_USD/BlackRock-GF-World-Healthscience-A2-%28USD%29.html";
 
             HtmlWeb web = new HtmlWeb();
 
             var htmlDoc = web.Load(html);
 
-            var node = htmlDoc.DocumentNode.SelectSingleNode("//tr[1]/td[2]");
+            //var nodes = htmlDoc.DocumentNode.SelectNodes("//script");
+
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//div[2]/div[2]/script[2]");
 
             Console.WriteLine(node.InnerText);
 
