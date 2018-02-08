2dab25d4230be9e53a06079488420326a6e83aa1
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-02-08 20:05:26 +0100

    Added new fields to Portfolio.cs. for Starting and  Maximum Price, and Stop Loss. Added more data to DbInitializer.cs

--- InvestmentFundsPortfolio/Data/DbInitializer.cs
+++ InvestmentFundsPortfolio/Data/DbInitializer.cs
@@ -18,22 +18,49 @@ namespace InvestmentFundsPortfolio.Data
             var funds = new Fund[]
             {
                 new Fund{Name="Skarbiec Obligacja Instrumentów Dłużnych (Skarbiec FIO)",
-                         Url=@"https://www.bankier.pl/fundusze/notowania/SKR05",
-                         Price=313.75m},
+                         Url=@"https://www.analizy.pl/fundusze/fundusze-inwestycyjne/profil-funduszu/SKR05/Skarbiec-Obligacja-Instrumentow-Dluznych-%28Skarbiec-FIO%29.html",
+                         Price=313.76m},
                 new Fund
                 {
                     Name = "Investor Nowych Technologii (Investor SFIO)",
-                    Url = @"https://www.bankier.pl/fundusze/notowania/INV36",
-                    Price=117.68m
+                    Url = @"https://www.analizy.pl/fundusze/fundusze-inwestycyjne/profil-funduszu/INV36/Investor-Nowych-Technologii-%28Investor-SFIO%29.html",
+                    Price=118.80m
                 },
                 new Fund
                 {
-                    Name = "",
-                    Url = @"",
-                    Price=0.0m
+                    Name = "Noble Fund Global Return",
+                    Url = @"https://www.analizy.pl/fundusze/fundusze-inwestycyjne/profil-funduszu/NOB07/Noble-Fund-Global-Return-%28Noble-Funds-FIO%29.html",
+                    Price=131.78m
+                },
+                new Fund
+                {
+                    Name = "Franklin Templeton BRIC Fund (USD)",
+                    Url = @"https://www.analizy.pl/fundusze/fundusze-zagraniczne/profil-funduszu/FTI035_A_USD/Templeton-BRIC-Fund-A-%28Acc%29-%28USD%29.html",
+                    Price=19.63m
+                },
+                new Fund
+                {
+                    Name = "UniAkcje Małych i Średnich Spółek",
+                    Url = @"https://www.analizy.pl/fundusze/fundusze-inwestycyjne/profil-funduszu/UNI19/UniAkcje-Malych-i-Srednich-Spolek-%28UniFundusze-FIO%29.html",
+                    Price=109.63m
+                },
+                new Fund
+                {
+                    Name = "UniFundusze UniAkcje: Turcja",
+                    Url = @"https://www.analizy.pl/fundusze/fundusze-inwestycyjne/profil-funduszu/UNI34/UniAkcje%3A-Turcja-%28UniFundusze-FIO%29.html",
+                    Price=67.35m
                 }
             };
+            foreach (Fund f in funds)
+            {
+                context.Funds.Add(f);
+            }
 
+            var portfolios = new Portfolio[]
+            {
+                new Portfolio {InvestorID = 1, FundID = 1, StartingPrice = 310.18m, MaxPrice = 314.21m, StopLoss = 2.0m},
+                new Portfolio {InvestorID = 1, FundID = 2, StartingPrice = 113.47m, MaxPrice = 125.26m, StopLoss = 8.0m}
+            };
         }
     }
 }
--- InvestmentFundsPortfolio/Data/Portfolio.cs
+++ InvestmentFundsPortfolio/Data/Portfolio.cs
@@ -5,6 +5,9 @@
         public int PortfolioID { get; set; }
         public int FundID { get; set; }
         public int InvestorID { get; set; }
+        public decimal StartingPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public decimal StopLoss { get; set; }
 
         public Fund Fund { get; set; }
         public Investor Investor { get; set; }
