5128da5776b1c8438c76237b146c31ccfcef80de
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-02-10 21:08:19 +0100

    Added method to get price of fund from external website.

--- HAPTests/Program.cs
+++ HAPTests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using HtmlAgilityPack;
 using System;
+using System.Globalization;
 
 namespace HAPTests
 {
@@ -10,18 +11,40 @@ namespace HAPTests
             // var html = @"https://www.bankier.pl/fundusze/notowania/UNI34";
 
            var html = @"https://www.analizy.pl/fundusze/fundusze-inwestycyjne/profil-funduszu/OPT04/89th-Avenue-Fund-No-1-FIZ.html";
-            html = @"https://www.analizy.pl/fundusze/fundusze-zagraniczne/profil-funduszu/BGF054_A2_USD/BlackRock-GF-World-Healthscience-A2-%28USD%29.html";
+            html = @"https://www.analizy.pl/fundusze/fundusze-zagraniczne/profil-funduszu/notowania/BGF054_A2_USD/BlackRock-GF-World-Healthscience-A2-%28USD%29.html";
 
             HtmlWeb web = new HtmlWeb();
 
             var htmlDoc = web.Load(html);
 
-            //var nodes = htmlDoc.DocumentNode.SelectNodes("//script");
+            var nodes = htmlDoc.DocumentNode.SelectNodes("//script");
 
-            var node = htmlDoc.DocumentNode.SelectSingleNode("//div[2]/div[2]/script[2]");
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//div[1]/div[2]/script[2]");
 
-            Console.WriteLine(node.InnerText);
+            var price = GetPrice(node.InnerText);
 
+            Console.WriteLine(price.ToString());
+
+
+
+
+        }
+
+        static decimal GetPrice(string text)
+        {
+            var texts = text.Split(' ');
+            
+
+            text = texts[48].Trim();
+
+            var value1Index = text.LastIndexOf("value1");
+
+            var tempText = text.Substring(value1Index,18);
+            texts = tempText.Split('"');
+
+            tempText = texts[2].Trim();
+
+            return decimal.Parse(tempText, new CultureInfo("en-US"));
         }
     }
 }
